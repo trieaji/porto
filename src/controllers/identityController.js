7655bf5a664ca95c98692475f2655804f2e70027
Laksa Bayu <laksabayu18.com>
2023-10-17 08:25:07 +0700

    add new code is update

--- src/controllers/identityController.js
+++ src/controllers/identityController.js
@@ -1,4 +1,4 @@
-import { identity } from "../services/identityService.js";
+import { identity, update } from "../services/identityService.js";
 
 const createIdentity = async (req,res,next) => {
     try {
@@ -15,6 +15,24 @@ const createIdentity = async (req,res,next) => {
     }
 }
 
+const updateIdentity = async (req,res,next) => {
+    try {
+        // console.log('=== req luur ===')
+        // console.log(req)
+        let prm = req.params.id
+        let img = req.files
+        img.id = prm
+        let request = req.body
+        const result = await update(request, img)
+        res.status(200).json({
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+} 
+
 export {
-    createIdentity
+    createIdentity,
+    updateIdentity
 }
