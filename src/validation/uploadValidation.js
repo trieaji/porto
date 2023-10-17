54e20e86eb2ba1ecf5c681a9a26ae8ce930eb67d
Laksa Bayu <laksabayu18.com>
2023-10-17 08:22:51 +0700

    add new code is update validation

--- src/validation/uploadValidation.js
+++ src/validation/uploadValidation.js
@@ -6,6 +6,14 @@ const uploadValidation = Joi.object({
     image_sim: Joi.string().max(255).required()
 })
 
+const updateUploadValidation = Joi.object({
+    id: Joi.number().min(1).positive().required(),
+    your_name: Joi.string().max(100).optional(),
+    image_ktp: Joi.string().max(255).required(),
+    image_sim: Joi.string().max(255).required()
+})
+
 export {
-    uploadValidation
+    uploadValidation,
+    updateUploadValidation
 }
